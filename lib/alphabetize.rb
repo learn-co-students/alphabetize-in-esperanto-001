aece5a9c98ef5ea9a977d3d3897f4138fa443fe4
github username <github email address>
2015-09-07 18:33:36 -0400

    alpha

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  hash = {}
+  alphabet.split("").each_with_index do |x,y|
+    hash[x] = y + 1
+  end
+  hash[" "] = 0
+
+  arr.sort_by do |word|
+    sum = ""
+    word.split("").each do |letter|
+      if hash[letter] < 10
+        sum += "0" + hash[letter].to_s
+      else
+        sum += hash[letter].to_s
+      end
+    end
+    sum = "." + sum
+    sum.to_f
+  end
 end
