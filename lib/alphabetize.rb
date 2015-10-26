8c02761dc4ee244ed952a7da34725dc0e142572d
maiya777 <maiya777@list.ru>
2015-10-26 19:18:58 +0300

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,25 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz "
+
+
+
+
 def alphabetize(arr)
-  # code here
+    
+        arr.sort_by {|item| i = 0
+        sum = ESPERANTO_ALPHABET.index(item[i])
+        if arr.index(item)+1 < arr.length
+          j = 1
+        else
+          j = -1
+        end
+        while item[i] == arr[arr.index(item)+j][i] 
+        i+=1
+        sum += ESPERANTO_ALPHABET.index(item[i])*0.01**i
+        end
+        sum     
+}
+  
+       # code here
 end
+
+
