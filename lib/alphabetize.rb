e1b288811180cf431c746aca6a167fdad349eb46
epang1 <epang1@gmail.com>
2016-02-14 15:52:59 -0500

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+no_match = false
+letter_counter = 0
+until no_match == true
+  if arr[0][letter_counter] == arr[1][letter_counter] && letter_counter < arr[0].length
+    letter_counter += 1
+  else
+    no_match = true
+  end
+end
+
+arr.sort_by do |word| 
+  alpha.index(word[letter_counter])
+end
+
 end
