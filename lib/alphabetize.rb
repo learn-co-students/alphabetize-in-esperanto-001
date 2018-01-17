5086510890f2aa3f5fbbea597ca05786129c2212
andrewkjankowski <andrewkjankowski@gmail.com>
2018-01-16 21:09:56 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
 def alphabetize(arr)
   # code here
+  sorted = arr.sort{ |a, b| 
+    index_array(a) <=> index_array(b)  
+  }
+end
+
+def index_array(str)
+  str.split("").collect{ |letter| esperanto_index(letter)}
+end
+
+def esperanto_index(ch)
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto_alphabet.index(ch)
 end
